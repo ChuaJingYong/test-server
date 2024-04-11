2cf1f9fe0c3e0308527daca2acc0f7ec3f8af6e5
Jing Yong <jyongcode@gmail.com>
2024-04-11 16:54:32 +0800

    updated PORT

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     const person = {
@@ -8,6 +9,6 @@ app.get('/', (req, res) => {
     res.json(person);
 });
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
-});
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
